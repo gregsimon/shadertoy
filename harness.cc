02bb1dad7d27a662751ccaae36a2664bf8448a85
Greg Simon <gregsimon@gmail.com>
2017-06-12 23:22:38 -0700

    finished gfx pipeline

--- harness.cc
+++ harness.cc
@@ -90,6 +90,7 @@ private:
 
 	VkRenderPass _renderPass;
 	VkPipelineLayout _pipelineLayout;
+	VkPipeline _graphicsPipeline;
 
 
 
@@ -293,6 +294,28 @@ private:
 		    throw std::runtime_error("failed to create pipeline layout!");
 		}
 
+
+		VkGraphicsPipelineCreateInfo pipelineInfo = {};
+		pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+		pipelineInfo.stageCount = 2;
+		pipelineInfo.pStages = shaderStages;
+		pipelineInfo.pVertexInputState = &vertexInputInfo;
+		pipelineInfo.pInputAssemblyState = &inputAssembly;
+		pipelineInfo.pViewportState = &viewportState;
+		pipelineInfo.pRasterizationState = &rasterizer;
+		pipelineInfo.pMultisampleState = &multisampling;
+		pipelineInfo.pDepthStencilState = nullptr; // Optional
+		pipelineInfo.pColorBlendState = &colorBlending;
+		pipelineInfo.pDynamicState = nullptr; // Optional
+		pipelineInfo.layout = _pipelineLayout;
+		pipelineInfo.renderPass = _renderPass;
+		pipelineInfo.subpass = 0;
+		pipelineInfo.basePipelineHandle = VK_NULL_HANDLE; // Optional
+		pipelineInfo.basePipelineIndex = -1; // Optional
+
+		if (vkCreateGraphicsPipelines(_device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &_graphicsPipeline) != VK_SUCCESS) {
+		    throw std::runtime_error("failed to create graphics pipeline!");
+		}
 
 
 
@@ -723,6 +746,7 @@ private:
   }
 
   void cleanup() {
+  	vkDestroyPipeline(_device, _graphicsPipeline, nullptr);
   	vkDestroyPipelineLayout(_device, _pipelineLayout, nullptr);
   	vkDestroyRenderPass(_device, _renderPass, nullptr);
 
