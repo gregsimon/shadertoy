e53b9de12e7afd3261ef525e527f03c94cf7c0ee
Greg Simon <gregsimon@gmail.com>
2017-06-12 23:04:19 -0700

    renderpass added

--- harness.cc
+++ harness.cc
@@ -87,6 +87,8 @@ private:
 
   VkShaderModule _vertShaderModule;
 	VkShaderModule _fragShaderModule;
+
+	VkRenderPass _renderPass;
 	VkPipelineLayout _pipelineLayout;
 
 
@@ -118,7 +120,44 @@ private:
     createLogicalDevice();
     createSwapChain();
     createImageViews();
+    createRenderPass();
     createGraphicsPipeline();
+  }
+
+  void createRenderPass() {
+  	VkAttachmentDescription colorAttachment = {};
+    colorAttachment.format = _swapChainImageFormat;
+    colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+		VkAttachmentReference colorAttachmentRef = {};
+		colorAttachmentRef.attachment = 0;
+		colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+		VkSubpassDescription subpass = {};
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.colorAttachmentCount = 1;
+		subpass.pColorAttachments = &colorAttachmentRef;
+
+		VkRenderPassCreateInfo renderPassInfo = {};
+		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+		renderPassInfo.attachmentCount = 1;
+		renderPassInfo.pAttachments = &colorAttachment;
+		renderPassInfo.subpassCount = 1;
+		renderPassInfo.pSubpasses = &subpass;
+
+		if (vkCreateRenderPass(_device, &renderPassInfo, nullptr, &_renderPass) != VK_SUCCESS) {
+		    throw std::runtime_error("failed to create render pass!");
+		}
+
+
+
   }
 
   void createGraphicsPipeline() {
@@ -685,6 +724,8 @@ private:
 
   void cleanup() {
   	vkDestroyPipelineLayout(_device, _pipelineLayout, nullptr);
+  	vkDestroyRenderPass(_device, _renderPass, nullptr);
+
 
   	for (size_t i = 0; i < _swapChainImageViews.size(); i++) {
       vkDestroyImageView(_device, _swapChainImageViews[i], nullptr);
