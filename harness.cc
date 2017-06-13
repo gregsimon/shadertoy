353e6e4728eabff2dff4f09e3ab75e0b0a17bff5
Greg Simon <gregsimon@gmail.com>
2017-06-12 20:55:08 -0700

    image views

--- harness.cc
+++ harness.cc
@@ -82,6 +82,8 @@ private:
   VkFormat _swapChainImageFormat;
   VkExtent2D _swapChainExtent;
 
+  std::vector<VkImageView> _swapChainImageViews;
+
 
   typedef std::vector<const char*> DeviceExtensionsList;
   const DeviceExtensionsList _deviceExtensions = {
@@ -109,7 +111,33 @@ private:
     pickPhysicalDevice();
     createLogicalDevice();
     createSwapChain();
+    createImageViews();
   }
+
+  void createImageViews() {
+  	_swapChainImageViews.resize(_swapChainImages.size());
+  	for (size_t i = 0; i < _swapChainImages.size(); i++) {
+  		VkImageViewCreateInfo createInfo = {};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = _swapChainImages[i];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = _swapChainImageFormat;
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+			if (vkCreateImageView(_device, &createInfo, nullptr, &_swapChainImageViews[i]) != VK_SUCCESS) {
+    		throw std::runtime_error("failed to create image views!");
+			}
+
+		}
+	}
+
 
   void createSwapChain() {
     SwapChainSupportDetails swapChainSupport = querySwapChainSupport(_physicalDevice);
@@ -476,6 +504,11 @@ private:
   }
 
   void cleanup() {
+  	for (size_t i = 0; i < _swapChainImageViews.size(); i++) {
+      vkDestroyImageView(_device, _swapChainImageViews[i], nullptr);
+    }
+
+
     vkDestroySwapchainKHR(_device, _swapChain, nullptr);
     vkDestroyDevice(_device, nullptr);
     DestroyDebugReportCallbackEXT(_instance, _callback, nullptr);
