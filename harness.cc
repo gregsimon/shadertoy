45062dba2a4bb50ba55f5fe3dcacb95ceae76b92
Greg Simon <gregsimon@gmail.com>
2017-06-14 19:48:33 -0700

    creeating framebuffers

--- harness.cc
+++ harness.cc
@@ -92,7 +92,7 @@ private:
 	VkPipelineLayout _pipelineLayout;
 	VkPipeline _graphicsPipeline;
 
-
+  std::vector<VkFramebuffer> _swapChainFramebuffers;
 
   typedef std::vector<const char*> DeviceExtensionsList;
   const DeviceExtensionsList _deviceExtensions = {
@@ -123,6 +123,30 @@ private:
     createImageViews();
     createRenderPass();
     createGraphicsPipeline();
+    createFramebuffers();
+  }
+
+  void createFramebuffers() {
+    _swapChainFramebuffers.resize(_swapChainImageViews.size());
+
+    for (size_t i = 0; i < _swapChainImageViews.size(); i++) {
+      VkImageView attachments[] = {
+          _swapChainImageViews[i]
+      };
+
+      VkFramebufferCreateInfo framebufferInfo = {};
+      framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+      framebufferInfo.renderPass = _renderPass;
+      framebufferInfo.attachmentCount = 1;
+      framebufferInfo.pAttachments = attachments;
+      framebufferInfo.width = _swapChainExtent.width;
+      framebufferInfo.height = _swapChainExtent.height;
+      framebufferInfo.layers = 1;
+
+      if (vkCreateFramebuffer(_device, &framebufferInfo, nullptr, &_swapChainFramebuffers[i]) != VK_SUCCESS) {
+          throw std::runtime_error("failed to create framebuffer!");
+      }
+    }
   }
 
   void createRenderPass() {
@@ -746,6 +770,10 @@ private:
   }
 
   void cleanup() {
+    for (size_t i = 0; i < _swapChainFramebuffers.size(); i++) {
+        vkDestroyFramebuffer(_device, _swapChainFramebuffers[i], nullptr);
+    }
+
   	vkDestroyPipeline(_device, _graphicsPipeline, nullptr);
   	vkDestroyPipelineLayout(_device, _pipelineLayout, nullptr);
   	vkDestroyRenderPass(_device, _renderPass, nullptr);
